345c8d275b6dddecb7b772954fbdce621f4a0069
Maximilian GREIFENEDER <GreifenederM22085@sus.htl-grieskirchen.at>
2026-06-07 18:23:42 +0200

    implemented route layout.svelte to bind in navbar

--- FrontendSchnellVerkauft/src/routes/+layout.svelte
+++ FrontendSchnellVerkauft/src/routes/+layout.svelte
@@ -1,9 +1,21 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
+
+	// Navbar auf Login/Register ausblenden
+	let hideNavbar = $derived(
+		$page.url.pathname === '/login' || $page.url.pathname === '/register'
+	);
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+{#if !hideNavbar}
+	<Navbar />
+{/if}
+
 {@render children()}
